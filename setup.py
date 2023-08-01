cb244f64d49c7d64582b8502b461edb712204fdc
mz <mnavahan2@gmail.com>
2023-08-01 12:56:46 +0330

    unstable 1

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 from setuptools import setup
-import envycontrol
+import envycontrol_VFIO
 
 
 setup(
-    name='envycontrol',
+    name='envycontrol_With_VFIO ',
     version=envycontrol.VERSION,
     description='Easy GPU switching for Nvidia Optimus laptops under Linux',
-    url='http://github.com/bayasdev/envycontrol',
-    author='Victor Bayas',
-    author_email='victorsbayas@gmail.com',
+    url='https://github.com/Maziar123/envycontrol_With_VFIO',
+    author='Victor Bayas with branch of Maziar ',
+    author_email='victorsbayas@gmail.com & maziar mnavahan3@gmail.com',
     license='MIT',
-    py_modules=['envycontrol'],
+    py_modules=['envycontrol_With_VFIO'],
     entry_points={
         'console_scripts': [
             'envycontrol=envycontrol:main',
@@ -19,7 +19,7 @@ setup(
     },
     keywords=['nvidia', 'optimus', 'prime', 'gpu', 'linux'],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: TST ONLY',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Operating System :: POSIX :: Linux'
